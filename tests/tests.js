e6908ea95db1f2a0c5b023a070eb07cec89b4b06
Alex <alex.g@spotinst.com>
2018-02-14 15:55:11 -0800

    basic logs test

--- tests/tests.js
+++ tests/tests.js
@@ -1,14 +1,21 @@
 const chai = require('chai');
-const deployIndex = require('../deploy/index.js');
+const LogsObject = require('../logs/index.js');
 const SpotinstObj = require('./utils/SpotinstServerlessObject');
 const Serverless = require('serverless');
+
+const assert = chai.assert;
+const expect = chai.expect;
+const assume = chai.should;
+
+let serverless = new Serverless();
+let logsObject = new LogsObject(serverless, {});
 
 // describe a test group
 describe("logs/index.js tests", () => {
   // describe a specific test
   it('logs()', (done) => {
-    let serverless = new Serverless();
-    
+    console.log(logsObject.logs());
+    assert.isNotNull(logsObject.logs(), 'logs is not null');
     
     done();
   });
